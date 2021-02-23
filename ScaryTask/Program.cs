0834d7793d46b7e4343b8edbfcb6efaa153990ed
Nikita Smolyaninov <nikita.smolaninov@gmail.com>
2021-02-23 21:41:10 +0200

    Main commit

---
+++ ScaryTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace ScaryTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach(var x in new FibSeq())
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+    class FibIterator : IEnumerator
+    {
+        private int RealNumber;
+        private int i;
+        private long cur = 0;
+        private long prev = 1;
+        public FibIterator(int Number)
+        {
+            RealNumber = Number;
+        }
+        public bool MoveNext()
+        {
+            if (i >= RealNumber)
+                return false;
+            (prev, cur) = (cur, prev + cur);
+            i++;
+            return true;
+        }
+        public void Reset()
+        {
+            cur = 0;
+            prev = 1;
+        }
+        public object Current
+        {
+            get
+            {
+                return cur;
+            }
+        }
+    }
+    class FibSeq : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return new FibIterator(56);
+        }
+    }
+}
---
+++ ScaryTask2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace ScaryTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (var x in new FibSeq2(56))
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+    class FibSeq2 : IEnumerable
+    {
+        private int RealNumber;
+
+        public FibSeq2(int Number)
+        {
+            RealNumber = Number;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            long cur = 0;
+            long prev = 1;
+            for (int i = 0; i < RealNumber; i++)
+            {
+                yield return cur;
+                (prev, cur) = (cur, cur + prev);
+            }
+        }
+    }
+}
